7c1b7095c82160654d9d77d763d7dde2222a9aed
DrEverr <ms1997sokol@gmail.com>
2024-09-28 12:41:19 +0200

    added providers to global

--- lib/utils/globals.dart
+++ lib/utils/globals.dart
@@ -1,8 +1,16 @@
 import 'package:example_drift_with_bloc/database/database.dart';
+import 'package:example_drift_with_bloc/provider/api_provider.dart';
+import 'package:example_drift_with_bloc/repository/history_repository.dart';
+import 'package:example_drift_with_bloc/repository/log_repository.dart';
 import 'package:get_it/get_it.dart';
+import 'package:repo/repo.dart';
 
 final getIt = GetIt.instance;
 
 void setupLocator() {
   getIt.registerSingleton(AppDatabase());
+  getIt.registerSingleton(Repo());
+  getIt.registerSingleton(ApiProvider(getIt<AppDatabase>(), getIt<Repo>()));
+  getIt.registerSingleton(LogRepository(getIt<AppDatabase>()));
+  getIt.registerSingleton(HistoryRepository(getIt<AppDatabase>(), getIt<ApiProvider>()));
 }
